c25f590d70d7330d54327bdfc9b87e6a72600e26
leaferjs <leaferjs@163.com>
2026-03-24 13:18:27 +0800

    fix(code)

--- examples/display/ImageBox/app/index.ts
+++ examples/display/ImageBox/app/index.ts
@@ -1,4 +1,4 @@
-// #创建 ImageBox [标准创建 (Leafer)]
+// #创建 ImageBox [标准创建 (App)]
 import { App, Ellipse } from 'leafer-ui'
 import '@leafer-in/editor' // 导入图形编辑器插件
 import '@leafer-in/viewport' // 导入视口插件 (可选)
