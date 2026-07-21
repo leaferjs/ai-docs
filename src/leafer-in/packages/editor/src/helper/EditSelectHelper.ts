cbb17347195471acf2dd9d9d002d3378f672616b
leaferjs <leaferjs@163.com>
2026-07-21 19:12:56 +0800

    v2.2.4

--- src/leafer-in/packages/editor/src/helper/EditSelectHelper.ts
+++ src/leafer-in/packages/editor/src/helper/EditSelectHelper.ts
@@ -7,34 +7,41 @@ export const EditSelectHelper = {
         return path.list.find((leaf) => leaf.editable) as IUI
     },
 
-    findByBounds(branch: IUI, bounds: IBounds): IUI[] {
+    findByBounds(branch: IUI, bounds: IBounds, mode: 'hit' | 'includes' = 'hit'): IUI[] {
         const list: IUI[] = []
-        eachFind([branch], list, bounds)
+        eachFind([branch], list, bounds, mode)
         return list
     }
 
 }
 
 
-function eachFind(children: IUI[], list: IUI[], bounds: IBounds): void {
-    let child: IUI, data: IUIData
+function eachFind(children: IUI[], list: IUI[], bounds: IBounds, mode: 'hit' | 'includes'): void {
+    let child: IUI, data: IUIData, isFind: boolean
     for (let i = 0, len = children.length; i < len; i++) {
         child = children[i], data = child.__
         if (data.hittable && data.visible && !data.locked && bounds.hit(child.__world)) {
 
-            if (data.editable) {
-                if (child.isBranch && !data.hitChildren) {
-                    if (data.hitSelf) list.push(child)
-                    continue
-                } else if (child.isFrame) {
+            if (data.editable && data.editable !== 'single') {
+                if (child.isFrame) {
                     if (bounds.includes(child.__layout.boxBounds, child.__world)) {
                         list.push(child)
                         continue
                     }
-                } else if (bounds.hit(child.__layout.boxBounds, child.__world) && data.hitSelf) list.push(child)
+                } else {
+
+                    isFind = bounds[mode](child.__layout.boxBounds, child.__world) && data.hitSelf
+
+                    if (child.isBranch) {
+                        if (!data.hitChildren) {
+                            if (isFind) list.push(child)
+                            continue
+                        }
+                    } else if (isFind) list.push(child)
+                }
             }
 
-            if (child.isBranch) eachFind(child.children, list, bounds)
+            if (child.isBranch) eachFind(child.children, list, bounds, mode)
 
         }
     }
